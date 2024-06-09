4f3ee6ac593e857c28a0770f26a8a0b775fa6f3b
SSebia <ssebia@outlook.com>
2024-06-09 21:03:28 +0300

    Token verify api

---
+++ src/main/java/lt/almantas/booksite/controllers/TokenController.java
@@ -0,0 +1,18 @@
+package lt.almantas.booksite.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/token")
+@Validated
+public class TokenController {
+    @PostMapping("/verify")
+    public ResponseEntity<Void> verifyToken(@RequestBody String token) {
+        // Nes JwtAuthenticationFilter jau patikrino tokeną, todėl čia nieko nedarysime
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
