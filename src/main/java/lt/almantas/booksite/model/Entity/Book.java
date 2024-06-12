3bdaabb7857918c58f14396b3174adb0c718fcb1
SSebia <ssebia@outlook.com>
2024-06-12 16:44:17 +0300

    Creating categories, editing categories, deleting categories, books too

--- src/main/java/lt/almantas/booksite/model/Entity/Book.java
+++ src/main/java/lt/almantas/booksite/model/Entity/Book.java
@@ -19,6 +19,8 @@ public class Book {
     private String title;
     private String description;
     private String isbn;
+    @Lob
+    @Column(length = 100000)
     private String base64;
     private int pages;
 
